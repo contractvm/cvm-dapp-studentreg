8db5b93841dd009e756dd3d88fc470b147df8ce0
Davide Gessa <gessadavide@gmail.com>
2015-11-30 18:59:07 +0100

    example update

--- samples/helloworld.py
+++ samples/helloworld.py
@@ -2,18 +2,15 @@
 # Copyright (c) 2015 Davide Gessa
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-from libcontractvm import Wallet, WalletNode, ConsensusManager
+from libcontractvm import Wallet, WalletNode, WalletChainSo, ConsensusManager
 from studentreg import StudentRegManager
 import sys
-import config
 import time
 
 consMan = ConsensusManager.ConsensusManager ()
 consMan.bootstrap ("http://127.0.0.1:8181")
 
-#wallet=WalletChainSo.WalletChainSo (wallet_file='data/test_xltnode_a.wallet')
-wallet=WalletNode.WalletNode (chain='XLT', url=config.WALLET_NODE_URL, wallet_file='data/test_xltnode_a.wallet')
-
+wallet = WalletChainSo.WalletChainSo (wallet_file='test.wallet')
 srMan = StudentRegManager.StudentRegManager (consMan, wallet=wallet)
 
 def check ():
--- samples/list.py
+++ samples/list.py
@@ -2,21 +2,20 @@
 # Copyright (c) 2015 Davide Gessa
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-from libcontractvm import Wallet, WalletNode, ConsensusManager
+from libcontractvm import Wallet, WalletExplorer, ConsensusManager
 from studentreg import StudentRegManager
 import sys
-import config
 import time
+import os
 
 consMan = ConsensusManager.ConsensusManager ()
 consMan.bootstrap ("http://127.0.0.1:8181")
 
-#wallet=WalletChainSo.WalletChainSo (wallet_file='data/test_xltnode_a.wallet')
-wallet=WalletNode.WalletNode (chain='XLT', url=config.WALLET_NODE_URL, wallet_file='data/test_xltnode_a.wallet')
-
+wallet = WalletChainSo.WalletChainSo (wallet_file='test.wallet')
 srMan = StudentRegManager.StudentRegManager (consMan, wallet=wallet)
 
 while True:
+	os.system ('clear')
 	print ('List:')
 	v = srMan.getList ()
 	for x in v:
